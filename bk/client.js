42e77333164fd25930d647659b674192ac92d8d0
fd3s0412 <fd3s0412@gmail.com>
2018-09-02 14:19:11 +0900

    add readme

---
+++ bk/client.js
@@ -0,0 +1,297 @@
+$(function() {
+	// ----------------------------------------------------------------------
+	// クライアントの処理を扱うクラス.
+	// ----------------------------------------------------------------------
+	function Client() {
+		// ユーザー一覧DOM
+		this.$userList = $('#userList');
+		// ユーザー名Dom
+		this.$userName = $('#userName');
+		// 確定ボタンDom
+		this.$kakuteiButton = $('#kakuteiButton');
+		// スタートボタンDom
+		this.$startButton = $('#startButton');
+		// 朝ボタンDom
+		this.$morningButton = $('#morningButton');
+		// 夜ボタンDom
+		this.$nightButton = $('#nightButton');
+		// 参加者クリアボタンDom
+		this.$clearButton = $('#clearButton');
+		// 通信用オブジェクト
+		this.socket = io.connect();
+		// イベント設定
+		this.setEvent();
+		// 初期処理
+		this.init();
+	}
+	// ----------------------------------------------------------------------
+	// 初期処理.
+	// ----------------------------------------------------------------------
+	Client.prototype.init = function() {
+		var userName = localStorage.getItem("qcJinrouUserName");
+		if (userName) {
+			this.$userName.val(userName);
+		}
+		var userId = localStorage.getItem("qcJinrouUserId");
+		if (userId) {
+			this.send('setSocketId', userId)
+		}
+	};
+	// ----------------------------------------------------------------------
+	// イベント設定.
+	// ----------------------------------------------------------------------
+	Client.prototype.setEvent = function() {
+		var self = this;
+		this.$kakuteiButton.click(function() {
+			self.clickKakuteiButton();
+		});
+		this.$clearButton.click(function() {
+			self.clickClearButton();
+		});
+		this.$startButton.click(function() {
+			self.clickStartButton();
+		});
+		this.$morningButton.click(function() {
+			self.clickMorningButton();
+		});
+		this.$nightButton.click(function() {
+			self.clickNightButton();
+		});
+		this.socket.on('showUsers', function(gameInfo) {
+			self.showUsers(gameInfo);
+		});
+		this.socket.on('clearId', function() {
+			localStorage.removeItem("qcJinrouUserId");
+		});
+		this.socket.on('showMessage', function(message) {
+			$('#messages').prepend($('<li>' + message + '</li>'));
+		});
+		this.socket.on('requestMorningAction', function(params) {
+			self.requestMorningAction(params);
+		});
+		this.socket.on('requestNightAction', function(params) {
+			self.requestNightAction(params);
+		});
+		this.socket.on('startTimer', function() {
+			self.startTimer();
+		});
+		this.socket.on('gameSet', function(winTeam) {
+			self.gameSet(winTeam);
+		});
+		this.socket.on('actionCompleted', function(completedIdList) {
+			self.actionCompleted(completedIdList);
+		});
+	};
+	// ----------------------------------------------------------------------
+	// 確定ボタン押下処理.
+	// ----------------------------------------------------------------------
+	Client.prototype.clickKakuteiButton = function() {
+		var params = {
+			userName : this.$userName.val()
+		};
+		var userId = localStorage.getItem("qcJinrouUserId");
+		if (userId)
+			params.userId = userId;
+		this.send('saveUser', params, function(result) {
+			localStorage.setItem("qcJinrouUserId", result.userId);
+			localStorage.setItem("qcJinrouUserName", result.userName);
+		});
+	};
+	// ----------------------------------------------------------------------
+	// 参加者クリアボタン押下処理.
+	// ----------------------------------------------------------------------
+	Client.prototype.clickClearButton = function() {
+		this.send('clear', this.$userName.val());
+		$('#gameMessage').empty();
+	};
+	// ----------------------------------------------------------------------
+	// ゲームスタートボタン押下処理.
+	// ----------------------------------------------------------------------
+	Client.prototype.clickStartButton = function() {
+		this.send('start', this.$userName.val());
+	};
+	// ----------------------------------------------------------------------
+	// 朝ボタン押下処理.
+	// ----------------------------------------------------------------------
+	Client.prototype.clickMorningButton = function() {
+		this.send('morning', this.$userName.val());
+	};
+	// ----------------------------------------------------------------------
+	// 夜ボタン押下処理.
+	// ----------------------------------------------------------------------
+	Client.prototype.clickNightButton = function() {
+		this.send('night', this.$userName.val());
+	};
+	// ----------------------------------------------------------------------
+	// サーバーにイベントキーを送信.
+	// ----------------------------------------------------------------------
+	Client.prototype.send = function(eventKey, params, callback) {
+		this.socket.emit(eventKey, params, callback);
+	};
+	// ----------------------------------------------------------------------
+	// 接続中のユーザーを表示.
+	// ----------------------------------------------------------------------
+	Client.prototype.showUsers = function(gameInfo) {
+		this.$userList.empty().append($(this.createTagUserList(gameInfo.userList)));
+
+		// ゲーム状態に応じてイベントを設定
+		switch (gameInfo.gameStatus) {
+			case "morning":
+				break;
+			case "night":
+				break;
+		}
+	};
+	// ----------------------------------------------------------------------
+	// ユーザー一覧のタグを作成.
+	// ----------------------------------------------------------------------
+	Client.prototype.createTagUserList = function(userList) {
+		var tag = "";
+		$.each(userList, function(i, d) {
+			console.log(d);
+			var addClass = "";
+			if (d.deadFlg) addClass = " dead";
+			if (d.morningActionFlg || d.nightActionFlg) addClass = " action_completed";
+			tag += '<li class="item_user' + addClass + '">';
+			if (d.imageName) {
+				tag += '<img src="/image/' + d.imageName + '" />';
+			}
+			tag += '<div class="item_name">' + d.userName + '</div>';
+			tag += '<input type="hidden" name="userId" value="' + d._id + '" />';
+			tag += '<input type="hidden" name="userName" value="' + d.userName + '" />';
+			tag += '<input type="hidden" name="yakushoku" value="' + d.yakushoku + '" />';
+			tag += '</li>';
+		});
+		return tag;
+	};
+	// ----------------------------------------------------------------------
+	// 朝の行動を開始させる.
+	// ----------------------------------------------------------------------
+	Client.prototype.requestMorningAction = function(params) {
+		var self = this;
+		var tag = '<h2>あなたの役職</h2><div class="mainMessage">' + params.message + '</div>';
+		tag += '<div class="subMessage">'+ params.addMessage + '</div>';
+		var $message = $(tag);
+		$message.hide();
+		$('#gameMessage').empty().append($message);
+		$message.fadeIn();
+		$(document).off('click', '.item_user');
+		$(document).on('click', '.item_user', function() {
+			console.log(params);
+			if (params.yakushoku === "占い師") {
+				var userName = $(this).find('[name="userName"]').val();
+				var yakushoku = $(this).find('[name="yakushoku"]').val();
+				var message = userName + 'さんは、 "' + yakushoku + '" です。';
+				alert(message);
+			} else {
+				alert("ご回答ありがとうございました。");
+			}
+			self.send('morningAction');
+			$(document).off('click', '.item_user');
+		});
+	};
+	// ----------------------------------------------------------------------
+	// 夜の行動を開始させる.
+	// ----------------------------------------------------------------------
+	Client.prototype.requestNightAction = function(params) {
+		var self = this;
+		var tag = '<h2>あなたの役職</h2><div class="mainMessage">' + params.message + '</div>';
+		tag += '<div class="subMessage">'+ params.addMessage + '</div>';
+		var $message = $(tag);
+		$message.hide();
+		$('#gameMessage').empty().append($message);
+		$message.fadeIn();
+		$(document).off('click', '.item_user');
+		$(document).on('click', '.item_user', function() {
+			var userId = $(this).find('[name="userId"]').val();
+			var userName = $(this).find('[name="userName"]').val();
+			alert(userName + "さんを選択しました。");
+			self.send('nightAction', {userId: userId, userName: userName});
+			$(document).off('click', '.item_user');
+		});
+	};
+	// ----------------------------------------------------------------------
+	// 勝敗判定.
+	// ----------------------------------------------------------------------
+	Client.prototype.gameSet = function(winTeam) {
+		var tag = '<h2>勝敗</h2><div class="mainMessage">' + winTeam + '</div>';
+		var $message = $(tag);
+		$message.hide();
+		$('#gameMessage').append($message);
+		$message.fadeIn(3000);
+		var $itemUser = $('.item_user');
+		$itemUser.click(function() {
+			var userId = $(this).find('[name="userId"]').val();
+			var userName = $(this).find('[name="userName"]').val();
+			alert(userName + "さんを選択しました。");
+			self.send('nightAction', {userId: userId, userName: userName});
+			$itemUser.off();
+		});
+	};
+	// ----------------------------------------------------------------------
+	// 行動完了状態を描画する処理.
+	// ----------------------------------------------------------------------
+	Client.prototype.actionCompleted = function(completedIdList) {
+		$('[name="userId"]').each(function(i, d) {
+			var $dom = $(d);
+			var userId = $dom.val();
+			var isExists = false;
+			$.each(completedIdList, function(i, id) {
+				if (id === userId) {
+					isExists = true;
+					return false;
+				}
+			});
+			var $li = $('[name="userId"][value="' + userId + '"]').parents('.item_user').first();
+			if (isExists) {
+				$li.addClass('action_completed');
+			} else {
+				$li.removeClass('action_completed');
+			}
+		});
+	};
+	// ----------------------------------------------------------------------
+	// タイマー.
+	// ----------------------------------------------------------------------
+	Client.prototype.startTimer = function() {
+		var self = this;
+
+		var countdown = function(due) {
+			var now = new Date();
+
+			var rest = due.getTime() - now.getTime();
+			var sec = Math.floor(rest / 1000 % 60);
+			var min = Math.floor(rest / 1000 / 60) % 60;
+			var hours = Math.floor(rest / 1000 / 60 / 60) % 24;
+			var days = Math.floor(rest / 1000 / 60 / 60 / 24);
+			var count = [ days, hours, min, sec ];
+
+			return count;
+		}
+
+		var goal = new Date();
+		goal.setMinutes(goal.getMinutes() + 3);
+
+		// console.log(countdown(goal));
+		var count = 0;
+		if (self.timer) {
+			clearInterval(self.timer);
+		}
+		self.timer = setInterval(function() {
+			count++;
+			if (count > (180 * 4)) {
+				timerOff();
+			}
+			var counter = countdown(goal);
+			var time = ('00' + counter[1]).slice(-2) + ':'
+			+ ('00' + counter[2]).slice(-2) + ':'
+			+ ('00' + counter[3]).slice(-2);
+			$('#timer').html(time);
+		}, 250);
+
+		function timerOff() {
+			clearInterval(self.timer);
+		}
+	};
+	new Client();
+});
